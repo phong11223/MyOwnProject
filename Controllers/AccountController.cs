79e292a5234329f6383c753a78a46e0d9071d095
phong11223 <56997334+phong11223@users.noreply.github.com>
2023-05-30 23:11:50 +0700

    ok

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using MyOwnProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyOwnProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MyProfile(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("MyProfile", model);
+            }
+
+            return View(model);
+
+        }
+
+        public ActionResult MyProfile()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
+
