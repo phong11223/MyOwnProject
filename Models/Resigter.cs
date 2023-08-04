a9395db6a9a992d18b3a20a6af3b38e75a81dc6b
phong11223 <56997334+phong11223@users.noreply.github.com>
2023-08-04 22:56:31 +0700

    Add files via upload

---
+++ Models/Resigter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyOwnProject.Models
+{
+    public class Resigter
+    {
+        [Required(ErrorMessage = "username must be entered")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "email address must be entered")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "mobile number must be entered")]
+        public int mobile { get; set; }
+        [Required(ErrorMessage = "date month and year must be entered")]
+        public DateTime dob { get; set; }
+        [Required(ErrorMessage = "city must be entered")]
+        public string city { get; set; }
+        [Required(ErrorMessage = "country must be entered")]
+        public string country { get; set; }
+    }
+}
